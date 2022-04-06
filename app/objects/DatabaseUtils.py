042e4a3a75e6f47e2fe3d72d3e075f16f11d2609
Julia <juliasbardelatti@gmail.com>
2022-04-06 15:45:28 -0300

    finais

--- app/objects/DatabaseUtils.py
+++ app/objects/DatabaseUtils.py
@@ -19,13 +19,12 @@ class database:
             database=par_db.get("database"),
             password=par_db.get("password"),
         )
-        self.query_por_estado   = "insert into HISTORICO_COVID_POR_ESTADOS(DATA_REFERENCIA,STATE ,DEATHS,TOTAL_CASES, NEW_DEATHS, NEW_CASES) values('{data_referencia}', '{state}', {deaths}, {total_cases},{new_deaths},{new_cases})"
-        self.query_totais       = "insert into HISTORICO_COVID_TOTAIS(DATA_REFERENCIA,DEATHS,TOTAL_CASES, NEW_DEATHS, NEW_CASES) values('{data_referencia}', {deaths}, {total_cases},{new_deaths},{new_cases})"
+        self.query_por_estado   = "insert into HISTORICO_COVID_POR_ESTADOS(DATA_REFERENCIA,STATE, NEW_DEATHS, NEW_CASES) values('{data_referencia}', '{state}',{new_deaths},{new_cases})"
+        self.query_truncate_estados = "TRUNCATE TABLE HISTORICO_COVID_POR_ESTADOS;"
         self.query_requests     = "insert into TOTAL_REQUESTS(DATA_REFERENCIA, QUANTIDADE, ENDPOINT) values('{data_referencia}', {quantidade}, '{endpoint}')"
         self.query_get_requests_by_endpoint_date = "SELECT * FROM TOTAL_REQUESTS WHERE DATA_REFERENCIA = '{data_referencia}' AND ENDPOINT = '{endpoint}'"
         self.query_update_requests = "UPDATE TOTAL_REQUESTS SET QUANTIDADE = {quantidade} WHERE DATA_REFERENCIA = '{data_referencia}' AND ENDPOINT = '{endpoint}';"
         self.query_get_requests_date = "SELECT ENDPOINT, QUANTIDADE FROM  TOTAL_REQUESTS WHERE DATA_REFERENCIA = '{data_referencia}';"
-        self.query_get_total = "SELECT * FROM  HISTORICO_COVID_TOTAIS;"
         self.query_get_total_estados = "SELECT * FROM  HISTORICO_COVID_POR_ESTADOS;"
 
     def execute_from_query(self, sql_file):
@@ -35,11 +34,7 @@ class database:
             cursor_cnxn_msql.execute(sqlScript)
 
     def ingest(self, values:dict):
-        if values['state'] != 'TOTAL':
-            insert_query = self.query_por_estado.format(**values)
-        else:
-            insert_query = self.query_totais.format(**values)
-        
+        insert_query = self.query_por_estado.format(**values)
         cursor_cnxn_msql = self.cnx_mysql.cursor()
         cursor_cnxn_msql.execute(insert_query)
         self.cnx_mysql.commit()
@@ -74,17 +69,6 @@ class database:
             cursor_cnxn_msql.execute(insert_query)
             self.cnx_mysql.commit()
 
-        
-
-
-    def ingest_requests(self, values:dict):
-        insert_query = self.query_requests.format(**values)
-        cursor_cnxn_msql = self.cnx_mysql.cursor(buffered=True)
-        cursor_cnxn_msql.execute(insert_query)
-        self.cnx_mysql.commit()
-
-    
-
     def get_requests(self, values:dict):
         insert_query = self.query_get_requests_date.format(**values)
         cursor_cnxn_msql = self.cnx_mysql.cursor(buffered=True, dictionary=True)
@@ -93,16 +77,6 @@ class database:
         self.cnx_mysql.commit()
         return valores_select
 
-
-    def get_total(self):
-        insert_query = self.query_get_total
-        cursor_cnxn_msql = self.cnx_mysql.cursor(buffered=True, dictionary=True)
-        cursor_cnxn_msql.execute(insert_query)
-        valores_select = cursor_cnxn_msql.fetchall()
-        self.cnx_mysql.commit()
-        return valores_select
-
-
     def get_total_estados(self):
         insert_query = self.query_get_total_estados
         cursor_cnxn_msql = self.cnx_mysql.cursor(buffered=True, dictionary=True)
@@ -110,3 +84,8 @@ class database:
         valores_select = cursor_cnxn_msql.fetchall()
         self.cnx_mysql.commit()
         return valores_select
+
+    def truncate_table_estados(self):
+        query = self.query_truncate_estados
+        cursor_cnxn_msql = self.cnx_mysql.cursor(buffered=True, dictionary=True)
+        cursor_cnxn_msql.execute(query)
--- app/script.py
+++ app/script.py
@@ -8,25 +8,23 @@ import requests
 from datetime import datetime
 import json
 import smtplib, ssl
+import numpy as np
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
-z = ZipFile('final.zip', 'r')
-z.extractall()
-z.close()
 
-df = pd.read_csv('dados.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv')
 
 # tratamento de colunas
-df.rename(columns={"newDeaths": "new_deaths", "newCases": "new_cases", "totalCases" : "total_cases"}, inplace=True)
+df.rename(columns={"newDeaths": "new_deaths", "newCases": "new_cases"}, inplace=True)
 df['data_referencia'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
 
 
 # agrupando e ordenando 
-df_final = df.groupby(['data_referencia','state']).sum()[["deaths", "total_cases", "new_deaths", "new_cases"]]
+df_final = df.groupby(['data_referencia','state']).sum()[["new_deaths", "new_cases"]]
 df_final = df_final.reset_index()
 df_final = df_final.sort_values(by='data_referencia')
 
-#df_estados = df_final[df_final["state"].str.contains("TOTAL")==False]
 
 data_engine = db.database()
 tabela_ja_existente = 0
@@ -52,7 +50,32 @@ if tabela_ja_existente == 0:
     inserir_dados_mysql(df_final, data_engine)
 
 
+
 app = Flask(__name__)
+
+
+@app.route("/atualizar/dados")
+def atualizar():
+    data_engine = db.database()
+
+    df = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv')
+    df.rename(columns={"newDeaths": "new_deaths", "newCases": "new_cases"}, inplace=True)
+    df['data_referencia'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+
+    df_final = df.groupby(['data_referencia','state']).sum()[["new_deaths", "new_cases"]]
+    df_final = df_final.reset_index()
+    df_final = df_final.sort_values(by='data_referencia')
+
+    data_engine.truncate_table_estados()
+
+    dados = df_final.shape[0]
+    for i in range(0, dados):
+        values = df_final.iloc[i].to_dict()
+        data_engine.ingest(values=values)
+
+    
+    return 'registros inseridos...'
+
 
 @app.route("/total/estados")
 def totalPorEstado():
@@ -70,22 +93,6 @@ def totalPorEstado():
     return json.dumps(total, indent=4, sort_keys=True, default=str)
 
 
-@app.route("/total")
-def total():
-    data_engine = db.database()
-    data_referencia = datetime.today().strftime('%Y-%m-%d')
-
-    values_request = {
-        "data_referencia": data_referencia,
-        "endpoint": "total"
-    }
-    
-    data_engine.add_requests(values=values_request)
-    total = data_engine.get_total()
-    return json.dumps(total, indent=4, sort_keys=True, default=str)
-
-
-
 @app.route("/requests")
 def requests():
     data_engine = db.database()
@@ -97,26 +104,32 @@ def requests():
     
     todos_requests = data_engine.get_requests(values=values_request)
 
-    #port = 465  # For SSL
-    #smtp_server = "smtp.gmail.com"
-    #sender_email = "sbardelatt@gmail.com"  
-    #receiver_email = "sbardelatt@gmail.com"  
-    #password = "****"
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "fiapgrupobranco@gmail.com"  
+    receiver_email = "fiapgrupobranco@gmail.com"  
+    password = "fiap7788"
 
-    #tipos_de_ingresso = []
+    message = "Subject: Report diário requests\n\n"
+    for tipo in todos_requests:
+        for k, v in tipo.items():
+            message += f"{k}: {v}"
+            message += "\n"
 
-    #message = "Subject: Report diário requests\n\n"
-    #for tipo in tipos_de_ingresso:
-    #    for k, v in tipo.items():
-    #        message += f"{k}: {v}\n"
-    #        message += "\n"
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
 
-    #context = ssl.create_default_context()
-    #with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    #    server.login(sender_email, password)
-    #    server.sendmail(sender_email, receiver_email, message.encode('utf-8'))
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.encode('utf-8'))
 
-    return json.dumps(todos_requests)
+
+    return 'email enviado com sucesso!'
+
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0", port=8080)
+
+
+
